ce1b3af4fcf065e87bb5f0821c705c0d27647558
paulywebster <paul.dubois.85@gmail.com>
2018-06-13 12:08:53 +0200

    refactor common factory

--- src/Common/CommonClientFactory.php
+++ src/Common/CommonClientFactory.php
@@ -3,33 +3,38 @@
 namespace FEIWebServicesClient\Common;
 
 use FEIWebServicesClient\Authentication\AuthenticationClientFactory;
-use FEIWebServicesClient\Authentication\Types\Login;
 use FEIWebServicesClient\Common\ClassMap\CommonWSClassmap;
+use FEIWebServicesClient\Environment;
+use FEIWebServicesClient\Service;
 use Phpro\SoapClient\ClientFactory as PhproClientFactory;
 use Phpro\SoapClient\ClientBuilder;
 
 class CommonClientFactory
 {
 
-    public static function factory(string $username, string $password) : \FEIWebServicesClient\Common\CommonClient
+    public static function factory(string $user) : \FEIWebServicesClient\Common\CommonClient
     {
-        $factory = AuthenticationClientFactory::factory('https://vdata.fei.org/_vti_bin/Authentication.asmx?WSDL');
-        $response = $factory->login(new Login($username, $password));
+        $config = require __DIR__.'/../../config/parameters.php';
+
+        $factory = AuthenticationClientFactory::factory($config['environment']);
+        $response = $factory->login($config['username'], $config['password']);
         if($response->getLoginResult()->getErrorCode() !== 'NoError')
         {
             throw new \Exception('Login failed !');
         }
         $cookies = $factory->getCookies();
 
+        $environment = Environment::fromString($config['environment']);
+        $service = Service::common();
         $clientFactory = new PhproClientFactory(CommonClient::class);
-        $clientBuilder = new ClientBuilder($clientFactory, 'https://vdata.fei.org/_vti_bin/FEI/CommonWS.asmx?WSDL', []);
+        $clientBuilder = new ClientBuilder($clientFactory, $environment->url().$service->uri(), []);
         $clientBuilder->withClassMaps(CommonWSClassmap::getCollection());
 
         $client = $clientBuilder->build();
         $header = new \SoapHeader(
             'http://fei.org/',
             'AuthHeader',
-            ['UserName' => $username, 'Language' => 'EN']
+            ['UserName' => $user, 'Language' => 'EN']
         );
         $client->applySoapHeaders($header);
         $client->setCookie('.CASXAUTH', $cookies['.CASXAUTH'][0]);
